d3a959245f284837254ed33b0e01df79d7f9c579
Jerry <lphyowai769@gmail.com>
2023-09-01 21:01:21 +0630

    Add browser.ts as entry point for webpack build

---
+++ src/browser.ts
@@ -0,0 +1,8 @@
+import RippleEffect from "./index";
+
+Object.defineProperty(window, "RippleEffect", {
+    value: RippleEffect,
+    writable: true,
+    configurable: true,
+    enumerable: false,
+});
